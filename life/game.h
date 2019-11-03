926af9b09cfe267a75a71ec62a6b5047ee5e8526
abna2071 <abna2071@colorado.edu>
2019-11-03 15:51:20 -0700

    setting up the .h and .cpp game files (included relevant files)

---
+++ life/game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QColor>
+#include <QWidget>
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+class Game : public QWidget, public QMainWindow {
+    Q_OBJECT
+public:
+
+};
+
+#endif // GAME_H
